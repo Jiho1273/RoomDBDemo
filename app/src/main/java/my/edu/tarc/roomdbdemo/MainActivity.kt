3a39ce6a6fb30902ca7cc963c7149420f1851d59
Ji Ho <soojiho127@gmail.com>
2021-08-26 17:25:17 +0800

    Initial commit

---
+++ app/src/main/java/my/edu/tarc/roomdbdemo/MainActivity.kt
@@ -0,0 +1,59 @@
+package my.edu.tarc.roomdbdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.launch
+import my.edu.tarc.roomdbdemo.data.Product
+import my.edu.tarc.roomdbdemo.data.ProductAdapter
+import my.edu.tarc.roomdbdemo.data.ProductDB
+import my.edu.tarc.roomdbdemo.data.ProductDao
+
+class MainActivity : AppCompatActivity() {
+    lateinit var dao: ProductDao
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        dao = ProductDB.getInstance(application).productDao
+
+        val btnInsert: Button = findViewById(R.id.btnInsert)
+        btnInsert.setOnClickListener() {
+
+            val name: String = findViewById<TextView>(R.id.tfName).text.toString()
+            val price: Double = findViewById<TextView>(R.id.tfPrice).text.toString().toDouble()
+
+            val p = Product(0, name, price)
+
+            CoroutineScope(IO).launch {
+                dao.insertProduct(p)
+            }
+
+        }
+
+        val btnGet: Button = findViewById(R.id.btnGet)
+        btnGet.setOnClickListener() {
+            CoroutineScope(IO).launch {
+
+                val productList = dao.getAll()
+
+                var nameList = ""
+                for (product: Product in productList) {
+                    nameList += product.name + "\n"
+                }
+
+                CoroutineScope(Main).launch {
+                    val productAdapter = ProductAdapter(productList)
+                    val rv : RecyclerView = findViewById(R.id.productRV)
+                    rv.adapter = productAdapter
+                    rv.setHasFixedSize(true)
+                }
+            }
+        }
+    }
+}
